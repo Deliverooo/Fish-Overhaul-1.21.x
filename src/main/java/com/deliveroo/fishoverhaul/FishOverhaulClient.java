16845e3e41997b1cfc504a0e854bf91af7a54c54
Deliverooo <ooconnor50@gmail.com>
2024-08-14 19:12:19 +0100

    Initial commit

---
+++ src/main/java/com/deliveroo/fishoverhaul/FishOverhaulClient.java
@@ -0,0 +1,10 @@
+package com.deliveroo.fishoverhaul;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FishOverhaulClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/deliveroo/fishoverhaul/FishOverhaulDataGenerator.java
@@ -0,0 +1,11 @@
+package com.deliveroo.fishoverhaul;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FishOverhaulDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
